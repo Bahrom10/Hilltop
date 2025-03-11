0f847abf344b8e1f6bbf8ffbab730115eeeed25a
Bahrom10 <bahodirovbahrom2010@gmail.com>
2025-03-11 22:04:31 +0500

    Deploy update

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,14 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import path from 'path';
+import staticPlugin from "vite-plugin-static"
 
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), staticPlugin()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
     },
   },
-  base: "/Hilltop"
+  base: "/Hilltop/"
 });
