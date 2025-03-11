0d16df4884386c4e140ccfd4870c238a5d70a6a0
Bahrom10 <bahodirovbahrom2010@gmail.com>
2025-03-11 22:13:45 +0500

    l

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,9 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import path from 'path';
-import staticPlugin from "vite-plugin-static"
 
 export default defineConfig({
-  plugins: [vue(), staticPlugin()],
+  plugins: [vue()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
